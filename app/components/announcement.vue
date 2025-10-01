74cb9ea21fcb4429d44d0406f33eca504e54c94a
mc506lw <3462689887@qq.com>
2025-10-01 14:34:45 +0800

    修复账号

--- app/components/announcement.vue
+++ app/components/announcement.vue
@@ -4,7 +4,7 @@
       <h3 class="font-bold text-lg">📢 网站公告</h3>
       <div class="py-4">
         <p class="pb-2">欢迎访问AQBManager！</p>
-        <p class="pb-2">此网站仅为a1.0.4版本的前端演示网站，所有功能均为模拟实现。</p>
+        <p class="pb-2">此网站为a1.0.4版本的前端演示网站，所有功能均为模拟实现。</p>
         <p class="pb-2">如您在使用过程中遇到任何问题，请不要感到奇怪，这很可能是由于：</p>
         <ul class="list-disc pl-5 pb-2">
           <li>演示网站的功能限制</li>
@@ -19,7 +19,7 @@
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                 d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
             </svg>
-            <span>请勿反馈任何此网站的问题或建议！</span>
+            <span>账号秘密均为admin</span>
           </div>
         </div>
       </div>
